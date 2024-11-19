b2fefe2aabebe99e7aec4508ff37a5036c4cb2e4
Tλrτarus <thortarus@gmail.com>
2024-11-19 16:50:28 +0100

    Ajout d'une partie map

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,16 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './core/header/header.component';
-import { MainComponent } from './main/main.component';
-import { FooterComponent } from './core/footer/footer.component';
-import { ContactComponent } from './contact/contact.component';
-import { SubscribeComponent } from './subscribe/subscribe.component';
 import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent, ContactComponent, SubscribeComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })

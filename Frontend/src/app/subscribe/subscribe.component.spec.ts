fcbfc1a4bc7813175fcd3874d4a31c15375fca62
Tλrτarus <thortarus@gmail.com>
2024-11-27 11:29:47 +0100

    Ajout pannel admin Start Bootstrap

--- Frontend/src/app/subscribe/subscribe.component.spec.ts
+++ Frontend/src/app/subscribe/subscribe.component.spec.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import {Validators} from '@angular/forms';
 
 export class AppComponent {
   userForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private http: HttpClient) {
-    this.userForm = this.fb.group({
+// Récupération des valeurs du formulaire.
+  constructor(private form: FormBuilder, private http: HttpClient) {
+
+    this.userForm = this.form.group({
       nom: [''],
       prenom: [''],
       email: [''],
       password: [''],
-      dateNaissance: [''],
+      dateNaissance: [''], 
       numTelephone: [''],
       isNotifSms: [''],
     });
   }
 
   onSubmit() {
+    // Conversion de l'objet en JSON.
     const formData = this.userForm.value;
+    // Envoi d'une requête HTTP POST à l'API /api/user.
     this.http.post('http://localhost:8000/api/user', formData)
+    // Gestion de la réponse du serveur (succès ou erreur).
       .subscribe(response => {
         console.log('Données envoyées avec succès', response);
       }, error => {
@@ -27,3 +33,7 @@ export class AppComponent {
       });
   }
 }
+
+
+
+
